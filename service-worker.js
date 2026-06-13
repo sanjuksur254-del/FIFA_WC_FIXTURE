1227c404d420b9ea0a81f273e8f538d50b5838fa
sanjuksur254-del <sanju.k.suresh254@gmail.com>
2026-06-13 16:23:38 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,23 @@
-const CACHE_NAME = "wc-fixture-v1";
+const CACHE_NAME = 'wc-companion-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png.png'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll([
-        "/FIFA_WC_FIXTURE/",
-        "/FIFA_WC_FIXTURE/index.html"
-      ]))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {});
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
